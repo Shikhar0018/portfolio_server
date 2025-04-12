ae03667027f179b60f354d85e91cca1708c3d466
shikharVayuz <86296418+ShikharVayuz@users.noreply.github.com>
2025-04-12 11:57:56 +0530

    Update database configuration to use environment variables and remove unused db service from Docker Compose

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,13 @@
 # app/database.py
+import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/app"
-
+# Use database url from your environment variables or configuration
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
